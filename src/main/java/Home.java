8337e332e965a78ab6db8f29612764ec9a8f20c1
leite <leite.maleck@outlook.com>
2016-01-23 03:29:45 +0100

    Initial commit

---
+++ src/main/java/Home.java
@@ -0,0 +1,23 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by leite_2 on 23/01/2016.
+ */
+public class Home extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+        resp.setContentType("index.jsq");
+    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
